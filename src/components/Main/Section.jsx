6e4f8de013b746455abd6c5b09a85f4b26413239
Todu66 <giga.todua06@gmail.com>
2023-04-25 23:52:05 +0400

    .

---
+++ src/components/Main/Section.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Header from "../Header/Header"
+import "./Section.css"
+class Section extends React.Component {
+    render () {
+        return (
+            <div className="section-container">
+                <Header />
+                <h1>dsf</h1>
+            </div>
+        )
+    }
+}
+
+export default Section;
