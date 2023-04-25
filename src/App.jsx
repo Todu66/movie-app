43e51b676fbe4753498c890b865edc361aadc0b3
Todu66 <giga.todua06@gmail.com>
2023-04-25 23:49:15 +0400

    main section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import { useState } from 'react'
-
+import { React, useState } from 'react'
 import './App.css'
-
+import Navigation from "./components/Navigation/Navigation";
+import Section from "./components/Main/Section"
 function App() {
-  
+  return (
+    <div className='mainHome'>
+      <Navigation />
+      <Section />
+    </div>
+    
+  )
   
 }
 
