be087ee0809d6b4ecad25c54500597ecb0c54f49
Todu66 <giga.todua06@gmail.com>
2023-05-29 17:45:48 +0400

    adding default width

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom'
+import { createRoot } from 'react-dom';
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const rootElement = document.getElementById('root');
+
+createRoot(rootElement).render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-)
+  </BrowserRouter>
+);
